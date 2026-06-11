1ceeeaaed0fcd63a9dfacc315d8e4014e0f88d2d
Daniel <daniel_tr31@hotmail.com>
2026-06-10 18:23:18 -0600

    add type to notebooks, remove parent_id from notes

--- app/models/notebooks.py
+++ app/models/notebooks.py
@@ -9,6 +9,7 @@ class Notebooks(Base):
     title = Column(String, nullable=False)
     description = Column(String, nullable=True)
     parent_id = Column(Integer, ForeignKey('notebooks.id'), nullable=True)
+    type = Column(String, nullable=False, default='notebook')
 
     # jerarquía
     parent = relationship('Notebooks', remote_side=[id], back_populates='children')
